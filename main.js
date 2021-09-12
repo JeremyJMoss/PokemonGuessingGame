54bbc7fc7495d80d8d9a54da304981046fcf58a7
JeremyJMoss <mossyj19@gmail.com>
2021-09-12 19:55:51 +1000

    first commit

---
+++ main.js
@@ -0,0 +1,308 @@
+/*storing data recieved from api in data*/
+let data;
+/*generates a random number based on range given*/
+let randomNumber = (range) => Math.ceil(Math.random() * range);
+/*storing random pokemon chosen in pokemonId*/
+let pokemonId = randomNumber(150);
+/*setting global var counter at 1 to create a switch/case stack for input function*/
+let counter = 1;
+/*getting all pokemon names in an array*/
+let allPokemonNames = [];
+let score = 20;
+let highscore = 0;
+let sortedPokemon;
+let hardModeOn = false;
+let p;
+let selectedPokemon = [];
+
+/*collecting items for manipulation from Dom*/
+const checkBtn = document.getElementById("check");
+const questionMark = document.getElementById("questionMark");
+const inputName = document.getElementById("nameInput");
+const infoSide = document.querySelector(".infoSide");
+const highScoreEl = document.querySelector(".highScore");
+const scoreEl = document.querySelector(".score");
+const bodyBackground = document.querySelector(".backgroundPopup");
+const popup = document.querySelector(".popUpWindow");
+const resetBtn = document.querySelector(".resetBtn");
+const infoBtn = document.getElementById("informationButton");
+const infoCont = document.querySelector(".pokemonInfo");
+const closeBtn = document.getElementById("closeBtn");
+const list = document.getElementById("pokemonList");
+const hardBtn = document.getElementById("hardBtn");
+const normalBtn = document.getElementById("normalBtn");
+const load = document.querySelector(".loadPage");
+
+/*creating empty object to fill with data from api*/
+let pokemon = {
+  type: [],
+  abilities: [],
+  moves: [],
+};
+
+/*creating a start state for reset button*/
+const startState = function () {
+  loadPage();
+  document.querySelector(".pokemonImage").remove();
+  hardModeOn === false
+    ? (pokemonId = randomNumber(150))
+    : (pokemonId = randomNumber(251));
+  counter = 1;
+  pokemon.type = [];
+  pokemon.abilities = [];
+  pokemon.moves = [];
+  getPokemon();
+  getInfo();
+  if (highscore < score) {
+    highscore = score;
+    highScoreEl.lastElementChild.innerText = highscore;
+  }
+  hardModeOn === false ? (score = 20) : (score = 40);
+  scoreEl.lastElementChild.innerText = score;
+  bodyBackground.style.display = "none";
+  popup.style.display = "none";
+  inputName.value = "";
+  let i = 0;
+  while ((i = infoSide.childNodes.length)) {
+    infoSide.firstChild.remove();
+    i++;
+  }
+};
+
+/*collecting data to store into pokemon object*/
+const getPokemon = function () {
+  axios
+    .get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+    .then((response) => {
+      console.log(response.data);
+      data = response.data;
+    })
+    .catch((err) => console.log("whoops something didn't work"));
+};
+
+/*retrieving data from api to push into 
+allPokemonNames array*/
+const getAllPokemon = function () {
+  axios
+    .get(`https://pokeapi.co/api/v2/pokemon?limit=251`)
+    .then((response) => {
+      return response.data;
+    })
+    .then((r) => {
+      return r.results;
+    })
+    .then((a) => {
+      for (let obj in a) {
+        allPokemonNames.push(a[obj].name);
+      }
+    })
+    .catch((err) => console.log("whoops something didn't work"));
+};
+
+/*hard mode function*/
+const modeSelect = function () {
+  loadPage();
+  if (hardModeOn === false) {
+    hardModeOn = true;
+    pokemonId = randomNumber(251);
+  } else {
+    hardModeOn = false;
+    pokemonId = randomNumber(151);
+  }
+  counter = 1;
+  pokemon.type = [];
+  pokemon.abilities = [];
+  pokemon.moves = [];
+  list.innerHTML = "";
+  getPokemon();
+  getInfo();
+  highscore = 0;
+  score != 40 ? (score = 40) : (score = 20);
+  highScoreEl.lastElementChild.innerText = highscore;
+  scoreEl.lastElementChild.innerText = score;
+  inputName.value = "";
+  let i = 0;
+  while ((i = infoSide.childNodes.length)) {
+    infoSide.firstChild.remove();
+    i++;
+  }
+  hardBtn.classList.toggle("hide");
+  normalBtn.classList.toggle("hide");
+};
+
+/*loading page to stop unwanted click events before api request has been made*/
+const loadPage = function () {
+  load.classList.toggle("hide");
+  setTimeout(function () {
+    load.classList.toggle("hide");
+  }, 1500);
+};
+/*creating list items to add to unordered list dynamically*/
+const createListItems = function () {
+  if (hardModeOn === false) {
+    for (let i = 0; i < 151; i++) {
+      p = document.createElement("p");
+      p.classList.add("createdListItem");
+      p.innerText = sortedPokemon[i];
+      list.appendChild(p);
+    }
+  } else {
+    for (let item of sortedPokemon) {
+      p = document.createElement("p");
+      p.classList.add("createdListItem");
+      p.innerText = item;
+      list.appendChild(p);
+    }
+  }
+};
+
+const selectedAm = function (value) {
+  for (let i = 0; i < value; i++) {
+    selectedPokemon.push(allPokemonNames[i]);
+  }
+};
+
+/*creating a div with whatever item i want in it*/
+const createDiv = function (value, objPos) {
+  const div = document.createElement("div");
+  div.classList.add("createdDiv");
+  div.innerHTML = `<p>${value} : ${objPos}</p>`;
+  infoSide.appendChild(div);
+};
+
+/*creating an image of pokemon stored in object*/
+const createImg = function (ev) {
+  const img = document.createElement("img");
+  img.setAttribute("src", pokemon.image);
+  img.classList.add("pokemonImage");
+  document.querySelector(".pokemonImageContainer").appendChild(img);
+};
+
+/*storing data from api into pokemon object*/
+const getInfo = function () {
+  setTimeout(function () {
+    pokemon.name = data.name;
+    pokemon.pokedexNumber = data.id;
+    pokemon.image = data.sprites.front_default;
+    pokemon.weight = data.weight.toString().slice(0, -1);
+    selectedPokemon = [];
+    hardModeOn === false ? selectedAm(151) : selectedAm(251);
+    sortedPokemon = selectedPokemon.sort();
+    createListItems();
+    for (let i = 0; i < data.types.length; i++) {
+      pokemon.type.push(data.types[i].type.name);
+    }
+    for (let i = 0; i < data.abilities.length; i++) {
+      pokemon.abilities.push(data.abilities[i].ability.name);
+    }
+    for (let i = 0; i < data.moves.length; i++) {
+      pokemon.moves.push(data.moves[i].move.name);
+    }
+    createDiv("primary type", pokemon.type[0]);
+  }, 1500);
+};
+
+/*creating a function to attach to event listener for checkBtn click*/
+const getInput = function (ev) {
+  ev.preventDefault();
+  if (allPokemonNames.includes(inputName.value.toLowerCase())) {
+    if (inputName.value.toLowerCase() === pokemon.name) {
+      createImg();
+      bodyBackground.style.display = "block";
+      popup.style.display = "flex";
+    } else {
+      switch (counter) {
+        case 1:
+          createDiv("first ability", pokemon.abilities[0]);
+          break;
+        case 2:
+          createDiv(
+            "learnable move",
+            pokemon.moves[randomNumber(pokemon.moves.length) - 1]
+          );
+          break;
+        case 3:
+          if (pokemon.abilities[1]) {
+            createDiv("second ability", pokemon.abilities[1]);
+            break;
+          } else {
+            counter++;
+          }
+        case 4:
+          createDiv(
+            "learnable move",
+            pokemon.moves[randomNumber(pokemon.moves.length) - 1]
+          );
+          break;
+        case 5:
+          if (pokemon.type[1]) {
+            createDiv("secondary type", pokemon.type[1]);
+            break;
+          } else {
+            counter++;
+          }
+        case 6:
+          if (pokemon.abilities[2]) {
+            createDiv("third ability", pokemon.abilities[2]);
+            break;
+          } else {
+            counter++;
+          }
+        case 7:
+          if (hardModeOn === true) {
+            pokemon.pokedexNumber <= 151
+              ? createDiv("region", "kanto")
+              : createDiv("region", "johto");
+            break;
+          } else {
+            counter++;
+          }
+        case 8:
+          createDiv("ends with", pokemon.name.charAt(-1));
+          break;
+        case 9:
+          createDiv("weight", `${pokemon.weight} Kg`);
+          break;
+        case 10:
+          createDiv("pokedex no.", pokemon.pokedexNumber);
+          break;
+        case 11:
+          createDiv(
+            "learnable move",
+            pokemon.moves[randomNumber(pokemon.moves.length) - 1]
+          );
+          break;
+        case 12:
+          createDiv("starts with", pokemon.name.charAt(0));
+      }
+      counter++;
+      score -= 1;
+      scoreEl.lastElementChild.innerText = score;
+      inputName.value = "";
+    }
+  } else {
+    hardModeOn === true
+      ? alert(`Please write a valid pokemon name from Kanto and Johto combined`)
+      : alert(`Please write a valid pokemon name
+    from the original 150`);
+  }
+};
+
+scoreEl.lastElementChild.innerText = score;
+highScoreEl.lastElementChild.innerText = highscore;
+
+loadPage();
+getAllPokemon();
+getPokemon();
+getInfo();
+
+checkBtn.addEventListener("click", getInput);
+resetBtn.addEventListener("click", startState);
+infoBtn.addEventListener("click", function () {
+  infoCont.style.display = "block";
+});
+closeBtn.addEventListener("click", function () {
+  infoCont.style.display = "none";
+});
+hardBtn.addEventListener("click", modeSelect);
+normalBtn.addEventListener("click", modeSelect);
