31d32a8a19c9837404a7bbfa609d10ac37ad4465
Jeremy J Moss <mossyj19@gmail.com>
2024-04-12 23:43:42 +1000

    made improvements

--- main.js
+++ main.js
@@ -19,68 +19,88 @@ const normalBtn = document.getElementById("normalBtn");
 const load = document.querySelector(".loadPage");
 const metaViewport = document.querySelector("meta[name=viewport]");
 
-/*storing data recieved from api in data*/
-let data;
-/*generates a random number based on range given*/
-let randomNumber = range => Math.ceil(Math.random() * range);
-/*storing random pokemon chosen in pokemonId*/
-let pokemonId = randomNumber(150);
+/* generates a random number based on range given */
+const getRandomNumber = function(range) {
+	return Math.ceil(Math.random() * range);
+} 
 /*setting global var counter at 1 to create a switch/case stack for input function*/
 let counter = 1;
 /*getting all pokemon names in an array*/
 let allPokemonNames = [];
 let score = 20;
 let highscore = 0;
-let sortedPokemon;
 let hardModeOn = false;
-let p;
-let selectedPokemon = [];
 /*viewport adjustment for keyboard issue on andoid*/
 let initialHeight = window.innerHeight;
 let initialWidth = window.innerWidth;
 
 /*creating empty object to fill with data from api*/
 let pokemon = {
-	type: [],
-	abilities: [],
-	moves: []
 };
 
-/*creating a start state for reset button*/
-const startState = function () {
-	loadPage();
-	checkBtn.addEventListener("click", getInput);
-	document.querySelector(".pokemonImage").remove();
-	hardModeOn
-		? (pokemonId = randomNumber(251))
-		: (pokemonId = randomNumber(150));
+const reset = function () {
+	document.querySelector(".pokemonImage")?.remove();
+	const pokemonId = hardModeOn ? getRandomNumber(251) : getRandomNumber(150);
 	counter = 1;
-	pokemon.type = [];
-	pokemon.abilities = [];
-	pokemon.moves = [];
 	list.innerHTML = "";
-	getPokemon();
-	getInfo();
-	if (highscore < score) {
-		highScoreEl.lastElementChild.innerText = highscore = score;
-	}
-	hardModeOn ? (score = 40) : (score = 20);
+	getPokemon(pokemonId)
+	.then((response) => {
+		getInfo(response);
+	})
+	.catch(err => {
+		console.log(err);
+	});
+	highscore = 0;
 	scoreEl.lastElementChild.innerText = score;
 	bodyBackground.style.display = "none";
 	popup.style.display = "none";
 	inputName.value = "";
 	infoSide.innerHTML = "";
+}
+
+/*creating a start state for reset button*/
+const startState = function () {
+	getAllPokemon()
+	.then(() => {
+		const pokemonId = hardModeOn ? getRandomNumber(251) : getRandomNumber(150);
+		counter = 1;
+		getPokemon(pokemonId)
+		.then((response) => {
+			getInfo(response);
+		})
+		.catch(err => {
+			console.log(err);
+		});
+		score = hardModeOn ? 40 : 20;
+		scoreEl.lastElementChild.innerText = score;
+		bodyBackground.style.display = "none";
+		popup.style.display = "none";
+		inputName.value = "";
+		infoSide.innerHTML = "";
+	})
+	.catch(err => {
+		console.log(err);
+	})
+	checkBtn.addEventListener("click", getInput);
+	resetBtn.addEventListener("click", startState);
+	infoBtn.addEventListener("click", () => (infoCont.style.display = "block"));
+	closeBtn.addEventListener("click", () => (infoCont.style.display = "none"));
+	hardBtn.addEventListener("click", modeSelect);
+	normalBtn.addEventListener("click", modeSelect);
 };
 
-/*collecting data to store into pokemon object*/
-const getPokemon = async function () {
+/* collecting data to store into pokemon object */
+const getPokemon = async function (pokemonId) {
 	try {
+		load.classList.toggle("hide");
 		const response = await axios.get(
 			`https://pokeapi.co/api/v2/pokemon/${pokemonId}`
 		);
-		data = response.data;
+		load.classList.toggle("hide");
+		const data = response.data;
+		return data;
 	} catch (err) {
-		console.log("whoops something didn't work");
+		throw err;
 	}
 };
 
@@ -88,76 +108,48 @@ const getPokemon = async function () {
 allPokemonNames array*/
 const getAllPokemon = async function () {
 	try {
+		load.classList.toggle("hide");
 		const response = await axios.get(
 			`https://pokeapi.co/api/v2/pokemon?limit=251`
 		);
-		response.data.results.forEach(obj => allPokemonNames.push(obj.name));
+		load.classList.toggle("hide");
+		allPokemonNames = response.data.results.map((obj) => {
+			return obj.name;
+		})
 	} catch (err) {
-		console.error(`Unable to find resource ${err}`);
+		throw err;
 	}
 };
 
 /*hard mode function*/
-const modeSelect = function () {
-	loadPage();
-	if (hardModeOn) {
-		hardModeOn = false;
-		pokemonId = randomNumber(151);
-	} else {
-		hardModeOn = true;
-		pokemonId = randomNumber(251);
-	}
-	counter = 1;
-	pokemon.type = [];
-	pokemon.abilities = [];
-	pokemon.moves = [];
-	list.innerHTML = "";
-	getPokemon();
-	getInfo();
-	highscore = 0;
-	score != 40 ? (score = 40) : (score = 20);
-	highScoreEl.lastElementChild.innerText = highscore;
-	scoreEl.lastElementChild.innerText = score;
-	inputName.value = "";
-	infoSide.innerHTML = "";
+const modeSelect = function () { 
+	hardModeOn = !hardModeOn;
+	score = hardModeOn ? 40 : 20;
+	reset();
 	hardBtn.classList.toggle("hide");
 	normalBtn.classList.toggle("hide");
 };
 
-/*loading page to stop unwanted click events before api request has been made*/
-const loadPage = function () {
-	load.classList.toggle("hide");
-	setTimeout(function () {
-		load.classList.toggle("hide");
-	}, 2000);
-};
-
 /*creating list items to add to unordered list dynamically*/
-const createListItems = function () {
-	if (hardModeOn) {
-		for (let item of sortedPokemon) {
-			p = document.createElement("p");
-			p.classList.add("createdListItem");
-			p.innerText = item;
-			list.appendChild(p);
-		}
-	} else {
-		for (let i = 0; i < 151; i++) {
-			p = document.createElement("p");
-			p.classList.add("createdListItem");
-			p.innerText = sortedPokemon[i];
-			list.appendChild(p);
-		}
+const createListItems = function (sortedPokemon) {
+	for (let pokemonName of sortedPokemon) {
+		const p = document.createElement("p");
+		p.classList.add("createdListItem");
+		p.innerText = pokemonName;
+		list.appendChild(p);
 	}
 };
 
-const selectedAm = function (value) {
-	for (let i = 0; i < value; i++) {
+const getSelectedPokemon = function (hardModeOn) {
+	const pokemonAmount = hardModeOn ? 251 : 151;
+	const selectedPokemon = [];
+	for (let i = 0; i < pokemonAmount; i++) {
 		selectedPokemon.push(allPokemonNames[i]);
 	}
+	return selectedPokemon.sort();
 };
 
-/*creating a div with whatever item i want in it*/
+/* creating a div with whatever item i want in it */
 const createDiv = function (value, objPos) {
 	const div = document.createElement("div");
 	div.classList.add("createdDiv");
@@ -165,7 +157,7 @@ const createDiv = function (value, objPos) {
 	infoSide.appendChild(div);
 };
 
-/*creating an image of pokemon stored in object*/
+/* creating an image of pokemon stored in object */
 const createImg = function (ev) {
 	const img = document.createElement("img");
 	img.setAttribute("src", pokemon.image);
@@ -173,28 +165,24 @@ const createImg = function (ev) {
 	document.querySelector(".pokemonImageContainer").appendChild(img);
 };
 
-/*storing data from api into pokemon object*/
-const getInfo = function () {
-	setTimeout(function () {
-		pokemon.name = data.name;
-		pokemon.pokedexNumber = data.id;
-		pokemon.image = data.sprites.front_default;
-		pokemon.weight = data.weight.toString().slice(0, -1);
-		selectedPokemon = [];
-		hardModeOn ? selectedAm(251) : selectedAm(151);
-		sortedPokemon = selectedPokemon.sort();
-		createListItems();
-		for (let i = 0; i < data.types.length; i++) {
-			pokemon.type.push(data.types[i].type.name);
-		}
-		for (let i = 0; i < data.abilities.length; i++) {
-			pokemon.abilities.push(data.abilities[i].ability.name);
-		}
-		for (let i = 0; i < data.moves.length; i++) {
-			pokemon.moves.push(data.moves[i].move.name);
-		}
-		createDiv("primary type", pokemon.type[0]);
-	}, 2000);
+/* storing data from api into pokemon object */
+const getPokemonInfo = function (data) {
+	pokemon.name = data.name;
+	pokemon.pokedexNumber = data.id;
+	pokemon.image = data.sprites.front_default;
+	pokemon.weight = data.weight.toString().slice(0, -1);
+	pokemon.type = data.types.map((type) => {
+		return type.type.name;
+	});
+	pokemon.abilities = data.abilities.map((ability) => {
+		return ability.ability.name;
+	});
+	pokemon.moves = data.moves.map((move) => {
+		return move.move.name;
+	});
+	createDiv("primary type", pokemon.type[0]);
+	const selectedPokemon = getSelectedPokemon(hardModeOn);
+	createListItems(selectedPokemon);
 };
 
 /*creating a function to attach to event listener for checkBtn click*/
@@ -214,7 +202,7 @@ const getInput = function (ev) {
 				case 2:
 					createDiv(
 						"learnable move",
-						pokemon.moves[randomNumber(pokemon.moves.length) - 1]
+						pokemon.moves[getRandomNumber(pokemon.moves.length) - 1]
 					);
 					break;
 				case 3:
@@ -227,7 +215,7 @@ const getInput = function (ev) {
 				case 4:
 					createDiv(
 						"learnable move",
-						pokemon.moves[randomNumber(pokemon.moves.length) - 1]
+						pokemon.moves[getRandomNumber(pokemon.moves.length) - 1]
 					);
 					break;
 				case 5:
@@ -246,9 +234,7 @@ const getInput = function (ev) {
 					}
 				case 7:
 					if (hardModeOn) {
-						pokemon.pokedexNumber <= 151
-							? createDiv("region", "kanto")
-							: createDiv("region", "johto");
+						createDiv("region", pokemon.pokedexNumber <= 151 ? "kanto" : "johto");
 						break;
 					} else {
 						counter++;
@@ -268,7 +254,7 @@ const getInput = function (ev) {
 				case 11:
 					createDiv(
 						"learnable move",
-						pokemon.moves[randomNumber(pokemon.moves.length) - 1]
+						pokemon.moves[getRandomNumber(pokemon.moves.length) - 1]
 					);
 					break;
 				case 12:
@@ -280,29 +266,14 @@ const getInput = function (ev) {
 			inputName.value = "";
 		}
 	} else {
-		hardModeOn
-			? alert(
-					`Please write a valid pokemon name from Kanto and Johto combined`
-			  )
-			: alert(`Please write a valid pokemon name
-    from the original 150`);
+		alert(`Please write a valid pokemon name from ${hardModeOn ? 'Kanto and Johto combined' : 'the original 150'}`);
 	}
 };
 
-scoreEl.lastElementChild.innerText = score;
-highScoreEl.lastElementChild.innerText = highscore;
 
-loadPage();
-getAllPokemon();
-getPokemon();
-getInfo();
 
-checkBtn.addEventListener("click", getInput);
-resetBtn.addEventListener("click", startState);
-infoBtn.addEventListener("click", () => (infoCont.style.display = "block"));
-closeBtn.addEventListener("click", () => (infoCont.style.display = "none"));
-hardBtn.addEventListener("click", modeSelect);
-normalBtn.addEventListener("click", modeSelect);
+startState();
+
 
 /*adjusting for mobile view resize for landscape and input field where keyboard comes in and ruins your day*/
 window.onresize = function () {
